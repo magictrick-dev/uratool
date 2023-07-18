cc12c528ebafdc1b5896ea200fa5c7b07d7f5b96
Chris DeJong <magiktrikdev@gmail.com>
2023-07-18 15:15:51 -0500

    Documentation

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,22 @@
+// -----------------------------------------------------------------------------
+// USB Replication Automation Tool  : URATool
+// Developed by Chris DeJong        : Github @ magictrick-dev
+// -----------------------------------------------------------------------------
+// URATool is a USB replication tool designed to run as a standalone linux service
+// to periodically replicate and backup USB devices on a schedule. As a TUI
+// application, it's designed to be ran both as a long-term application or as a
+// service. The original idea behind this application is to allow this to run on
+// a low power device such as a R.Pi mounted to the wall with a monitor and a USB
+// hub to attach USB drives to. These drives run on a backup after-hours which
+// doubles as a physical backup system but also to prevent people from removing
+// the drives from the backup routine.
+
 #include <iostream>
 #include <string>
 #include <cstring>
 
 #include <unistd.h>
+#include <signal.h>
 #include <libudev.h>
 
 #include <core/primitives.h>
@@ -10,42 +24,113 @@
 #include <gui_thread.h>
 #include <udev_thread.h>
 
+// -----------------------------------------------------------------------------
+// Application State Definition & Application Utilities
+// -----------------------------------------------------------------------------
+// TODO(Chris): As far as I know, the signal handler API doesn't allow for a way
+// to pass user data over without using some global. The application state is
+// fetched using a function fetcher/singleton instance such that we can access
+// it when a SIGINT event is caught.
+
+void exit_runtime(); // Forward dec.
+
+struct application_state
+{
+    ThreadingManager    thread_manager;
+    GUIThread*          gui_thread;
+    UDEVThread*         udev_thread;
+
+    udev*               udev_context;
+};
+
+inline static application_state*
+get_state()
+{
+    static application_state _instance = {};
+    return &_instance;
+}
+
+void
+signal_handler(int s)
+{
+    application_state* state = get_state();
+	state->gui_thread->set_runtime_state(false);
+    state->gui_thread->join();
+    exit_runtime();
+}
+
+void
+exit_runtime()
+{
+    application_state* state = get_state();
+	pthread_cancel(state->udev_thread->get_handle());
+	udev_unref(state->udev_context);
+    exit(0);
+}
+
+// -----------------------------------------------------------------------------
+// Main Definition
+// -----------------------------------------------------------------------------
+
 int
 main(int argc, char** argv)
 {
+
+    // -------------------------------------------------------------------------
+    // Initialize the application state.
+    // -------------------------------------------------------------------------
+    application_state* state = get_state();
+
+    // -------------------------------------------------------------------------
+    // Capture terminal signals.
+    // -------------------------------------------------------------------------
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = signal_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
 	// -------------------------------------------------------------------------
 	// Initialize the GUI thread.
 	// -------------------------------------------------------------------------
 
-	ThreadingManager thread_manager;
-	GUIThread* gui_thread = thread_manager.create_thread<GUIThread>();
-	gui_thread->set_runtime_state(true);
-	gui_thread->launch();
+	state->gui_thread = state->thread_manager.create_thread<GUIThread>();
+	state->gui_thread->set_runtime_state(true);
+	state->gui_thread->launch();
 
 	// -------------------------------------------------------------------------
 	// Initialize the UDEV thread.
 	// -------------------------------------------------------------------------
 
-    udev* udev_context = udev_new();
+    state->udev_context = udev_new();
 
-	UDEVThread* udev_thread = thread_manager.create_thread<UDEVThread>();
-	udev_thread->set_gui_thread(gui_thread); // Pass the GUI thread to the UDEV thread.
-	udev_thread->set_udev_context(udev_context); // Set a udev context for the thread.
-	udev_thread->launch();
+	state->udev_thread = state->thread_manager.create_thread<UDEVThread>();
+	state->udev_thread->set_gui_thread(state->gui_thread); // Pass the GUI thread to the UDEV thread.
+	state->udev_thread->set_udev_context(state->udev_context); // Set a udev context for the thread.
+	state->udev_thread->launch();
 
 	// -------------------------------------------------------------------------
 	// Continue running while the GUI is open.
 	// -------------------------------------------------------------------------
-	gui_thread->join();
+    // TODO(Chris): Our scheduling will probably happen in the main thread. The
+    // GUI thread should only handle front-end interaction and the UDEV thread
+    // contains UDEV-related operations. Timing, however, isn't not something either
+    // of these threads are responsible for, and since the main thread is open to
+    // be used outside of being a spin-loop (for now, just joins the GUI thread),
+    // it makes sense to have it periodically check the time and perform given
+    // operations as they're made.
+	state->gui_thread->join();
+
+    while (0)
+    {
+        // TODO(Chris): Our timing implementation goes here.
+    }
 
 	// -------------------------------------------------------------------------
-	// If the GUI thread has closed, we can force-kill the UDEV thread.
-	// You normally would not want this, because this potentially leaks
-	// resources. In our cases, this shouldn't cause issues since we will
-	// release these resources back to the OS as soon as the application closes.
+    // Exit runtime.
 	// -------------------------------------------------------------------------
-	pthread_cancel(udev_thread->get_handle());
-	udev_unref(udev_context);
+    exit_runtime();
 
 }
--- source/udev_thread.cpp
+++ source/udev_thread.cpp
@@ -9,57 +9,6 @@
 
 #include <sstream>
 
-#if 0
-/**
- * We will need to enumerate the devices on udev startup to get the currently
- * active devices. We won't know what's in, what's out, without first doing this.
- * Otherwise, we will need to force the user to unplug and replug to make it work.
- */
-
-void
-uratool_enumerate_devices(uratool_udev_instance* instance)
-{
-
-	udev_enumerate* enum_inst = udev_enumerate_new(instance->context);
-	
-	udev_enumerate_add_match_subsystem(enum_inst, "block");
-	udev_enumerate_add_match_property(enum_inst, "DEVTYPE", "partition");
-	
-	udev_enumerate_scan_devices(enum_inst);
-	udev_list_entry* devices = udev_enumerate_get_list_entry(enum_inst);
-	
-	udev_list_entry* device_entry;
-	udev_list_entry_foreach(device_entry, devices)
-	{
-		const char* path = udev_list_entry_get_name(device_entry);
-	
-		if (path)
-		{
-			udev_device* block_device = udev_device_new_from_syspath(instance->context, path);
-			udev_device* usb_device = udev_device_get_parent_with_subsystem_devtype(block_device, "usb", "usb_device");
-			
-			if (block_device && usb_device)
-			{
-				printf("---------------------------\n");
-				printf("DEVICE: %s\n", udev_device_get_sysname(block_device));
-				printf("DEVNAME: %s\n", udev_device_get_property_value(block_device, "DEVNAME"));
-				printf("DEVTYPE: %s\n", udev_device_get_property_value(block_device, "DEVTYPE"));
-				printf("VENDOR: %s\n", udev_device_get_sysattr_value(usb_device, "idVendor"));
-				printf("SERIAL: %s\n", udev_device_get_property_value(block_device, "ID_SERIAL_SHORT"));
-				printf("\n");
-			}
-
-			URATOOL_UDEV_UNREF(block_device, udev_device_unref);
-		}
-
-	}
-
-
-	URATOOL_UDEV_UNREF(enum_inst, udev_enumerate_unref);
-
-}
-#endif
-
 void UDEVThread::
 set_gui_thread(GUIThread* gui_thread)
 {
@@ -247,7 +196,7 @@ main()
     udev_enumerate_unref(enum_inst);
 
 	// -------------------------------------------------------------------------
-	// Initialize UDEV Monitor
+	// Initialize UDEV monitor.
 	// -------------------------------------------------------------------------
 
 	// Create the monitor.
@@ -261,12 +210,30 @@ main()
 	fcntl(fd, F_SETFL, udev_monitor_fd_flags & ~O_NONBLOCK);
 
 	// -------------------------------------------------------------------------
-    //
-	// Main runtime
+	// Main runtime.
 	// -------------------------------------------------------------------------
+    // The UDEV monitor doesn't busy-spin when waiting on devices; it will block
+    // until a device is caught and is ready to be processed. Because of this,
+    // the UDEV thread doesn't close in the typical behavior we'd expect. We aren't
+    // relying on the GUI thread's runtime state flag to be true every cycle since
+    // the loop doesn't cycle very often.
+    //
+    // The UDEV thread is force-closed by the main thread when the GUI thread
+    // exits using pthread_cancel(). Normally, you wouldn't want to do this since
+    // resources may not be freed, however there are mechanisms in place to clean
+    // up should the thread be forced to close.
+    //
+    // Essentially, main will lock the loop in case an iteration is in process.
+    // Then it will run the exit() routine which cleans up dynamically allocated
+    // resources and then exit.
 
 	while (this->_gui_thread->get_runtime_state())
 	{
+
+        // TODO(Chris): We should use a mutex lock to check if the thread is about
+        // to close. The main thread will lock that mutex during this process,
+        // preventing the runtime from continuing. It will then free resources
+        // and then close.
 
 		// We will poll the devices from our monitor.
 		struct udev_device* event_device = udev_monitor_receive_device(monitor);
